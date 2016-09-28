03f6b4a004b50067f22b15a3c2b3ced57ff3ff65
0x64796c616e <dmurphymancini@gmail.com>
2016-09-28 07:36:18 -0400

    update

--- Boan/mainwindow.cpp
+++ Boan/mainwindow.cpp
@@ -5,18 +5,26 @@
 
 #include <QFileSystemModel>
 #include <QTreeView>
+#include <QStandardItemModel>
 
 MainWindow::MainWindow(QWidget *parent) :
     QMainWindow(parent),
     ui(new Ui::MainWindow) {
     ui->setupUi(this);
 
-    QFileSystemModel *model = new QFileSystemModel;
-    model->setRootPath(QDir::currentPath());
-    QTreeView *tree = ui->treeView;
-    tree->setModel(model);
+    //QFileSystemModel *model = new QFileSystemModel;
+    //model->setRootPath(QDir::currentPath());
 
+    QStandardItemModel *model = new QStandardItemModel;
+    QStandardItem *parentItem = model->invisibleRootItem();
+    for (int i = 0; i < 4; ++i) {
+        QStandardItem *item = new QStandardItem(QString("item %0").arg(i));
+        parentItem->appendRow(item);
+        parentItem = item;
+    }
 
+    ui->treeView->setModel(model);
+    ui->treeView->show();
 }
 
 MainWindow::~MainWindow() {
